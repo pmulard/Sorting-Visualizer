ebc6c9d4ec8db278aec24e47b57888b0d0050321
pmully94 <peter.mulard@yahoo.com>
2020-06-26 23:36:05 -0700

    Initializes page with SortingVisualizer component. Component creates and displays an array.

---
+++ src/SortingVisualizer/SortingVisualizer.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import './SortingVisualizer.css';
+
+export default class SortingVisualizer extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            array: [],
+        };
+    }
+
+    // When app loads for the first time
+    componentDidMount() {
+        this.resetArray();
+    }
+
+    resetArray() {
+        const array = [];
+        for (let i=0; i < 100; i++) {
+            array.push(getRandomInt(5, 500))
+        }
+        this.setState({array});
+    }
+
+    render() {
+        const {array} = this.state;
+
+        return (
+            <div>
+                {array.map((element, index) => (
+                    <div className="array-bar" key={index}>
+                        {element}
+                    </div>
+                ))}
+            </div>
+        );
+    }
+}
+
+// From https://www.w3schools.com/js/js_random.asp
+// Min and Max are both inclusive.
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
