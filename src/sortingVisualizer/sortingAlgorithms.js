1b8235ad9df0270a59450591ddb1cd1d26bc44b6
pmully94 <peter.mulard@yahoo.com>
2020-06-30 20:51:01 -0700

    fixes shell sort bug that didn't correctly color some bars

--- src/sortingVisualizer/sortingAlgorithms.js
+++ src/sortingVisualizer/sortingAlgorithms.js
@@ -1,7 +1,7 @@
 var compareColor = 'aqua';
 var defaultColor = 'orange';
 var sortedColor = 'purple';
-var secondCompareColor = 'green';
+var secondCompareColor = 'yellowgreen';
 
 // SELECTION SORT
 export const selectionSort = (array) => {   
@@ -80,7 +80,7 @@ export const shellSort = (array) => {
             for (let barIndex=i; barIndex < array.length; barIndex+=space) {
                 barIndexArray.push(barIndex);
             }
-            animations.push([barIndexArray, compareColor, 'COMPARING'])
+            animations.push([barIndexArray, secondCompareColor, 'COMPARING'])
 
             const last = array.length-1;
             for (let j = i+space; j <= last; j+=space) {
@@ -90,9 +90,14 @@ export const shellSort = (array) => {
                 while (index >= 0 && unsortedElement < array[index]) {
                     array[index+space] = array[index];
                     // Changes color and values for bars as they shift
-                    animations.push([index, array[index], index+space, array[index+space], secondCompareColor, 'SHIFTING'])
-                    // Resets color of bars back to compareColor now that the swap is completed
-                    animations.push([index, index+space, compareColor, 'DONE_SHIFTING'])
+                    animations.push([index, array[index], index+space, array[index+space], compareColor, 'SHIFTING'])
+                    // Resets color of bars back to compareColor now that the swap is completed,
+                    // unless space=1, in which it is colored as completed
+                    if (space === 1) {
+                        animations.push([index, index+space, sortedColor, 'DONE_SHIFTING'])
+                    } else {
+                        animations.push([index, index+space, secondCompareColor, 'DONE_SHIFTING'])
+                    }
                     index -= space;
                 }
 
@@ -100,8 +105,14 @@ export const shellSort = (array) => {
                 // Changes the height of the last bar in iteration to reflect
                 // unsorted element being put in it's correct place
                 animations.push([index+space, array[index+space], 'LAST_SHIFT'])
-                // Changes the color of bars to sorted when on the final pass
+                // Changes the color of the last bar to be sorted when on the 
+                // final pass
                 if (space === 1) {
+                    // Changes color to sorted if the first element is properly sorted
+                    // on the very last pass when space=1
+                    if (index === 0) {
+                        animations.push([index, sortedColor, 'SORTED'])
+                    }
                     animations.push([index+space, sortedColor, 'SORTED'])
                 }
             }
@@ -115,18 +126,6 @@ export const shellSort = (array) => {
         space = Math.floor(space/2);
     }
     return animations;
-}
-
-export const shellSortIncrementSort = (array, space, first, last) => {
-    for (let j = first+space; j <= last; j+=space) {
-        let unsortedElement = array[j]
-        let index = j-space
-        while (index >= 0 && unsortedElement < array[index]) {
-            array[index+space] = array[index]
-            index -= space
-        }
-        array[index+space] = unsortedElement
-    }
 }
 
 
