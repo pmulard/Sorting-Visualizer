a9740449d5b72484e3d7c91190f690a34e2acf04
pmully94 <peter.mulard@yahoo.com>
2020-08-30 16:51:25 -0700

    adds build folder; remove /build from gitignore

---
+++ build/precache-manifest.4c0bf9b581bd6fb165eb196fc3bedaa1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f44ca2ebf82d20a38035c763bcfe9cc3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d5353bf34a834343a752",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "f6a3c7a07f427da7d134",
+    "url": "/static/css/main.f0b0a5c3.chunk.css"
+  },
+  {
+    "revision": "d5353bf34a834343a752",
+    "url": "/static/js/2.b21d9d87.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.b21d9d87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6a3c7a07f427da7d134",
+    "url": "/static/js/main.63c17df2.chunk.js"
+  },
+  {
+    "revision": "24b69844ae199ac3d391",
+    "url": "/static/js/runtime-main.d3d98d44.js"
+  }
+]);
