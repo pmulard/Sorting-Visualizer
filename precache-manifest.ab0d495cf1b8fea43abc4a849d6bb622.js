d1e3db98e706c0e6df230e5cb0b697f0c4615c07
pmully94 <peter.mulard@yahoo.com>
2020-08-31 12:53:24 -0700

    Updates

---
+++ precache-manifest.ab0d495cf1b8fea43abc4a849d6bb622.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "336c85e74bb05c4bd96b38ba9bc77f4e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "00e3c53ce7b3d684af97",
+    "url": "/static/css/2.43079d8c.chunk.css"
+  },
+  {
+    "revision": "f6a3c7a07f427da7d134",
+    "url": "/static/css/main.f0b0a5c3.chunk.css"
+  },
+  {
+    "revision": "00e3c53ce7b3d684af97",
+    "url": "/static/js/2.b21d9d87.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.b21d9d87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6a3c7a07f427da7d134",
+    "url": "/static/js/main.63c17df2.chunk.js"
+  },
+  {
+    "revision": "24b69844ae199ac3d391",
+    "url": "/static/js/runtime-main.d3d98d44.js"
+  }
+]);
